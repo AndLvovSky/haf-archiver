eb85e232c821124a75ddcdaaffbd8ab42a955863
AndLvovSky <viktorilvovskyi@ukr.net>
2019-05-08 12:13:07 +0300

    Add archiving GUI

--- app/main_window.cpp
+++ app/main_window.cpp
@@ -1,14 +1,55 @@
 #include "main_window.h"
 #include "ui_main_window.h"
 
+#include <QFileDialog>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
-    ui(new Ui::MainWindow)
-{
+    ui(new Ui::MainWindow) {
     ui->setupUi(this);
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
     delete ui;
+}
+
+void MainWindow::on_actionArchiving_triggered() {
+    auto stw = ui->stackedWidget;
+    stw->setCurrentIndex(1);
+}
+
+void MainWindow::on_chooseFilesToArchiveButton_clicked() {
+    QFileDialog dialog(this);
+    dialog.setFileMode(QFileDialog::ExistingFiles);
+    QStringList fileNames;
+    if (dialog.exec()) {
+        fileNames = dialog.selectedFiles();
+        ui->filesToArchiveText->setText(fileNames.join(";"));
+        updateReadyToArchive();
+    }
+}
+
+void MainWindow::on_chooseSaveDirectoryButton_clicked() {
+    QFileDialog dialog(this);
+    dialog.setFileMode(QFileDialog::Directory);
+    QString directoryName;
+    if (dialog.exec()) {
+        directoryName = dialog.selectedFiles()[0];
+        ui->saveDirectoryText->setText(directoryName);
+        updateReadyToArchive();
+    }
+}
+
+void MainWindow::on_archiveButton_clicked()
+{
+    // do archiving
+    ui->archiveButton->setEnabled(false);
+    QMessageLogger().debug("Start archiving");
+}
+
+void MainWindow::updateReadyToArchive() {
+    if (ui->filesToArchiveText->text() != "" &&
+        ui->saveDirectoryText->text() != "") {
+        ui->archiveButton->setEnabled(true);
+    }
 }
--- app/main_window.h
+++ app/main_window.h
@@ -7,15 +7,32 @@ namespace Ui {
 class MainWindow;
 }
 
-class MainWindow : public QMainWindow
-{
+class MainWindow : public QMainWindow {
     Q_OBJECT
 
+private:
+
+    void updateReadyToArchive();
+
+private slots:
+
+    void on_actionArchiving_triggered();
+
+    void on_chooseFilesToArchiveButton_clicked();
+
+    void on_chooseSaveDirectoryButton_clicked();
+
+    void on_archiveButton_clicked();
+
 public:
+
     explicit MainWindow(QWidget *parent = nullptr);
+
     ~MainWindow();
 
 private:
+
+
     Ui::MainWindow *ui;
 };
 
